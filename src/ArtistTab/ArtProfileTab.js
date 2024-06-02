d50633598092cce51404a154613428f81cba9151
sayaliwalde25 <sayaliwalde25@gmail.com>
2024-06-02 13:50:05 +0530

    done

--- src/ArtistTab/ArtProfileTab.js
+++ src/ArtistTab/ArtProfileTab.js
@@ -4,20 +4,61 @@ import { Card, Col, Row } from "react-bootstrap";
 
 const ArtProfileTab = ({ data }) => {
   return (
-    <div className="">
+    <div className="ArtProfileTab">
       {console.log(data)}
 
-      <h4 className="profile-h4">ArtProfileTab</h4>
-      <div className="profile-div">
-        <Card className="profile-card">
-          <Row className="profile-p">
-            <Col>Adhar Card Image: </Col>
+      <div className="ArtPofileTab-container">
+        <div className="ArtPofileTab-cover">
+      <Card >
+          <p >
+            <span>Name:</span> 
+            {data?.ArtistFullName}
+          </p>
+          <p >
+            <span>Address:</span>
+            {data?.ArtistAddress}
+          </p>
+          <p >
+            <span>City:</span> 
+            {data?.ArtistCity}
+          </p>
+          <p >
+            <span>Mobile No: </span>
+            {data?.ArtistMobileNo}
+          </p>
+          <p >
+            <span>Email:</span> 
+            {data?.ArtistEmail}
+          </p>
+          <p >
+            <span>State:</span> 
+            {data?.ArtistState}
+          </p>
+          <p >
+            <span>Pin Code: </span>
+            {data?.ArtistPincode}
+          </p>
+          <p >
+            <span>Adhar Card No:</span> 
+            {data?.ArtistAdharCardNo}
+          </p>
+          <p >
+            <span>IsVerified: </span>
+            {data?.IsVerified}
+          </p>
+        </Card>
+        <Card >
+          <Row >
+            <div className="front">
+              <Col>Adhar Card Image: </Col>
             <Col>
               {" "}
               <img src={`http://localhost:5000${data?.ArtistAdharCardImage}`} />
             </Col>
+            </div>
           </Row>
-          <Row className="profile-p">
+          <Row >
+            <div className="back">
             <Col> Handicap Certificate Image:</Col>
             <Col>
               {" "}
@@ -25,41 +66,10 @@ const ArtProfileTab = ({ data }) => {
                 src={`http://localhost:5000${data?.ArtistHandicapCertificateImage}`}
               />
             </Col>
+            </div>
           </Row>
         </Card>
-        <Card className="profile-cards">
-          <p className="profile-p">
-            <span>Name:</span> {data?.ArtistFullName}
-          </p>
-          <p className="profile-p">
-            <span>Address:</span>
-            {data?.ArtistAddress}
-          </p>
-          <p className="profile-p">
-            <span>City:</span> {data?.ArtistCity}
-          </p>
-          <p className="profile-p">
-            <span>Mobile No: </span>
-            {data?.ArtistMobileNo}
-          </p>
-          <p className="profile-p">
-            <span>Email:</span> {data?.ArtistEmail}
-          </p>
-          <p className="profile-p">
-            <span>State:</span> {data?.ArtistState}
-          </p>
-          <p className="profile-p">
-            <span>Pin Code: </span>
-            {data?.ArtistPincode}
-          </p>
-          <p className="profile-p">
-            <span>Adhar Card No:</span> {data?.ArtistAdharCardNo}
-          </p>
-          <p className="profile-p">
-            <span>IsVerified: </span>
-            {data?.IsVerified}
-          </p>
-        </Card>
+        </div>
       </div>
     </div>
   );
